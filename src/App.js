aeb4722e92859dbba65f9be99df024ee8ca94b4f
Lassi Chen <lachen@utu.fi>
2023-02-21 21:12:24 +0200

    2.6 done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,35 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName
+    }
+
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}>
+            {person.name}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
