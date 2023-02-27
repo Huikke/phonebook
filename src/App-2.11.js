99f34ddfca1d7706e449579c3592d9dee669bf9f
Lassi Chen <lachen@utu.fi>
2023-02-27 11:02:38 +0200

    2.11 done

---
+++ src/App-2.11.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      const person = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(person))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} />
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, handleNameChange, handleNumberChange, newName, newNumber }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>name: <input value={newName} onChange={handleNameChange} /></div>
+      <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      {persons.map(person =>
+        <Person key={person.name} name={person.name} number={person.number}/>
+      )}
+    </div>
+  )
+}
+
+const Person = ({ name, number }) => <div>{name} {number}</div>
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App-2.10'
+import App from './App-2.11'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
